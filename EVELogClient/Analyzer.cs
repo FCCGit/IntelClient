25d3ab14db4683f59ed8bcb8b4bc4f221219973d
Taylor Jones <monitorjbl@gmail.com>
2012-10-01 21:02:30 -0400

    initial commit

---
+++ EVELogClient/Analyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace EVELogServer
+{
+    class Analyzer
+    {
+        public static readonly string REGEX_SYSTEM_BARE = "( )[a-zA-Z0-9]{1,4}-[a-zA-Z0-9]{1,4}";
+        public static readonly string REGEX_SYSTEM_FQN = "( )[a-zA-Z0-9]{1,4}-[a-zA-Z0-9]{1,4}( )Solar System";
+        public static readonly string REGEX_CHARACTER = "[a-zA-Z0-9-_]{0,24}( )?[a-zA-Z0-9-_]{0,24}(( )( ))?";
+
+        public static void parse(string message)
+        {
+            Match bare = System.Text.RegularExpressions.Regex.Match(message, REGEX_SYSTEM_BARE);
+            Match fqn = System.Text.RegularExpressions.Regex.Match(message, REGEX_SYSTEM_FQN);
+
+            //if we cant pin this to a system, its useless
+            if (bare.Success || fqn.Success)
+            {
+                string system;
+                if (fqn.Success)
+                {
+                    system = fqn.Value;
+                }
+                else
+                {
+                    system = bare.Value;
+                }
+
+                string next = message.Replace(system, "");
+
+            }
+        }
+    }
+}
---
+++ EVELogClient/Report.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using agsXMPP;
+using agsXMPP.protocol.client;
+using agsXMPP.Xml.Dom;
+
+namespace EVELogClient
+{
+    class Report
+    {
+        private static XmppClientConnection xmppClient;
+
+        static Report()
+        {
+            init();
+        }
+
+        private static void init()
+        {
+            xmppClient = new XmppClientConnection(Properties.JABBER_URL, Properties.JABBER_PORT);
+            xmppClient.Open(Properties.JABBER_USER, Properties.JABBER_PASS);
+
+            //wait for connection to complete
+            int i = 0;
+            while (xmppClient.XmppConnectionState != XmppConnectionState.SessionStarted && i < Int32.Parse(Properties.getProperty("JABBER_TIMEOUT")))
+            {
+                Thread.Sleep(1000);
+                i++;
+            }
+
+            if (xmppClient.XmppConnectionState != XmppConnectionState.SessionStarted)
+            {
+                Console.WriteLine("Could not establish connection to Jabber server, exiting in three seconds.");
+                Thread.Sleep(3000);
+                Environment.Exit(1);
+            }
+        }
+
+
+        
+        public static void reportViaXMPP(string message)
+        {
+            if (xmppClient.XmppConnectionState != XmppConnectionState.SessionStarted)
+            {
+                Console.WriteLine("Lost Jabber connection, trying to reestablish...");
+            }
+            xmppClient.Send(new Message("ryshar@jabber.fatal-ascension.com/Home", MessageType.chat, message));
+        }
+    }
+}
